4faf636b069f83fbf088879cd61ebc00e468c987
lakhan018 <Dushantpariskar@gmail.com>
2025-01-16 11:20:47 +0530

    first progra,#

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// This is a basic Java program
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
