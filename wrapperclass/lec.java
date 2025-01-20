e76e86263e6425cced4a4e2b7449278b2d70bbe1
Dushant Pariskar <dushant.pariskar@example.com>
2025-01-20 09:59:22 +0530

    wrapper class

---
+++ wrapperclass/lec.java
@@ -0,0 +1,10 @@
+public class lec {
+ public static void main(String[] args) {
+    Integer myInt=5;
+    Double myDouble=4.4;
+    Character mychar='A';
+    System.out.println(myInt);
+    System.out.println(mychar);
+    System.out.println(myDouble);
+ }
+}
